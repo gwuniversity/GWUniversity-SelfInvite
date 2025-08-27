dc677cde8b2e09c0841de0a1ab66e44e1891ec4f
Chuck Baughn <51661151+cgbaughn@users.noreply.github.com>
2025-08-27 16:56:29 +0000

    Remove the RaiseHigh print from python and add it to else OUTPUT is null

--- .github/workflows/AutoInviteToOrgByNewIssue.py
+++ .github/workflows/AutoInviteToOrgByNewIssue.py
@@ -26,7 +26,6 @@ USERNAME = data["issue"]["user"]["login"] # assumes this value is always present
 # print(USERNAME)
 
 if "RAISEHIGH" not in COMMENT.upper():
-  print ("Required data not found in issue title")
   sys.exit()
 else:
 
